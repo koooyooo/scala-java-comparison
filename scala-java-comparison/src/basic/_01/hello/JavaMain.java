6e54d42ac5c6b03b20de3cd6e099ab7d618f9fa0
koyo <jab.cross.hook@gmail.com>
2015-11-18 11:38:07 +0900

    Mainの記述を追加

--- scala-java-comparison/src/basic/_01/hello/JavaMain.java
+++ scala-java-comparison/src/basic/_01/hello/JavaMain.java
@@ -1,10 +1,16 @@
 package basic._01.hello;
 
 /**
+ * Javaの Hello World
  * 
- * @author 
+ * @author K.Onda
  */
 public class JavaMain {
+	/**
+	 * 標準的な mainメソッド
+	 * 
+	 * @param args 引数
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
--- scala-java-comparison/src/basic/_01/hello/ScalaMain.scala
+++ scala-java-comparison/src/basic/_01/hello/ScalaMain.scala
@@ -1,6 +1,13 @@
 package basic._01.hello
 
+/**
+ * Scalaの HelloWorld
+ */
 object ScalaMain {
+  /**
+   * 標準的な Mainメソッド
+   * 
+   */
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
   }
