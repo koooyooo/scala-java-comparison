54943812328f73f368054d87fd2c48762516852c
koooyooo <jab.cross.hook@gmail.com>
2015-11-18 11:24:19 +0900

    最初のコミット

---
+++ scala-java-comparison/src/basic/_01/hello/JavaMain.java
@@ -0,0 +1,11 @@
+package basic._01.hello;
+
+/**
+ * 
+ * @author 
+ */
+public class JavaMain {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ scala-java-comparison/src/basic/_01/hello/ScalaMain.scala
@@ -0,0 +1,7 @@
+package basic._01.hello
+
+object ScalaMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala")
+  }
+}
