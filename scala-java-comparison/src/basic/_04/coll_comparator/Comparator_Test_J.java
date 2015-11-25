621676dcbfa5b13250552cd3fcb9480dbd72a8da
koyo <jab.cross.hook@gmail.com>
2015-11-25 12:22:21 +0900

    メソッド参照のコンパレータを追加

--- scala-java-comparison/src/basic/_04/coll_comparator/Comparator_Test_J.java
+++ scala-java-comparison/src/basic/_04/coll_comparator/Comparator_Test_J.java
@@ -1,6 +1,7 @@
 package basic._04.coll_comparator;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,7 +43,7 @@ public class Comparator_Test_J {
 	 * [API 8以降]
 	 * 関数を利用して実現したソート (関数の実装は Comparatorと同等)
 	 * 
-	 * シナリオ：人物の情報が格納されたリストを年齢でソートする
+	 * シナリオ：(同上)
 	 * 着眼点： 関数を利用することで、コードは簡素化された。
 	 * 		  但し、8以前で利用した様な汎用的なComparatorは無いので、比較処理を使い回す際には
 	 *        Function自体を変数として宣言しておき、適宜参照を配分させる必要がある。
@@ -65,6 +66,71 @@ public class Comparator_Test_J {
 				new Person("Alice", 20),
 				new Person("James", 35),
 				new Person("Tonny", 50))));
+	}
+	
+	/**
+	 * [API 8以降]
+	 * 関数を利用して実現したソート (メソッド参照により実現)
+	 * 
+	 * シナリオ：(同上)
+	 * 着眼点： comparator.compare と同等の役割を持つメソッドの参照により参照
+	 */
+	@Test
+	public void test3() {
+		final List<Person> personList = 
+				Arrays.asList(
+						new Person("Alice", 20),
+						new Person("James", 35),
+						new Person("Tonny", 50),
+						new Person("Jack",  15));
+		
+		// static method(arg1, arg2) の場合、比較対象が arg1, arg2に割り当てられる。
+		final List<Person> sortedListA =
+				personList.stream().sorted(Person::isOlderThanStatic).collect(Collectors.toList());
+		
+		assertThat(sortedListA, is(Arrays.asList(
+				new Person("Jack",  15),
+				new Person("Alice", 20),
+				new Person("James", 35),
+				new Person("Tonny", 50))));
+		
+		// instance obj1.method(arg1) の場合、比較対象が obj1, arg1に割り当てられる。
+		final List<Person> sortedListB =
+				personList.stream().sorted(Person::isOlderThan).collect(Collectors.toList());
+		
+		assertThat(sortedListB, is(Arrays.asList(
+				new Person("Jack",  15),
+				new Person("Alice", 20),
+				new Person("James", 35),
+				new Person("Tonny", 50))));		
+	}
+	
+	/**
+	 * [API 8以降]
+	 * 関数を利用して実現したソート (反転版のComparatorを簡易作成)
+	 * 
+	 * シナリオ：(同上)
+	 * 着眼点： comparator.compare と同等の役割を持つメソッドの参照により参照
+	 */
+	public void test4() {
+		final List<Person> personList = 
+				Arrays.asList(
+						new Person("Alice", 20),
+						new Person("James", 35),
+						new Person("Tonny", 50),
+						new Person("Jack",  15));
+		
+		final Comparator<Person> yougerComparator = (person1, person2) -> person1.isOlderThan(person2);
+		final Comparator<Person> olderComparator  = yougerComparator.reversed();
+		
+		final List<Person> sortedList =
+				personList.stream().sorted(olderComparator).collect(Collectors.toList());
+		
+		assertThat(sortedList, is(Arrays.asList(
+				new Person("Tonny", 50),
+				new Person("James", 35),
+				new Person("Alice", 20),
+				new Person("Jack",  15))));
 	}
 	
 }
--- scala-java-comparison/src/basic/_04/coll_comparator/Person.scala
+++ scala-java-comparison/src/basic/_04/coll_comparator/Person.scala
@@ -6,4 +6,25 @@ package basic._04.coll_comparator
  * @param name 名前
  * @param age 年齢
  */
-case class Person (val name: String, val age:Int)
+case class Person (val name: String, val age:Int) {
+  
+  /**
+   * 年齢が上であるかを判定する。
+   * 
+   * @param other 比較対象の Person
+   * @return 自身の年齢が上であれば true
+   */
+  def isOlderThan(other: Person): Int = {
+    this.age - other.age
+  }  
+}
+
+/**
+ * 人物を表現するクラスの コンパニオンオブジェクト(Javaの staticに近い責務を負う Object)
+ * 
+ */
+object Person {
+  def isOlderThanStatic(one: Person, other: Person): Int = {
+    one.age - other.age
+  }  
+}
